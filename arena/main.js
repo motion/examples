5fffb63c440f0f2a1c2f1327a187e516bf3da187
natew <natewienert@gmail.com>
2015-10-15 12:46:25 -0700

    start arena example

---
+++ arena/main.js
@@ -0,0 +1,66 @@
+view Main {
+  let project
+
+  <Project if={project} id={project} />
+  <Home if={!project} onSelectProject={p => project = p} />
+}
+
+view Home {
+  let fetched = false
+  let user = {}
+  let projects = []
+
+  load()
+
+  async function load() {
+    user = await fetchJSON(api.user('414'))
+    projects = await fetchJSON(api.user.channels('414'))
+    fetched = true
+  }
+
+  <loading if={!fetched}>
+    Loading...
+  </loading>
+  <home if={fetched}>
+    <h1>{user.full_name}</h1>
+    <img src={user.avatar} />
+    <h2>seecoy@me.com</h2>
+
+    <projects repeat={projects}>
+      {_}
+    </projects>
+  </home>
+}
+
+view Project {
+  let fetched = false
+  let contents = []
+
+  load()
+
+  async function load() {
+    // fetch data from arena
+    let data = await fetchJSON(api.channel(^id))
+    contents = data.contents
+    fetched = true
+  }
+
+  <loading if={!fetched}>
+    Loading...
+  </loading>
+  <contents if={fetched} repeat={contents}>
+    <h1>{_.title}</h1>
+    <img if={_.image} src={_.image.display.url} />
+  </contents>
+}
+
+
+// api
+
+export const api = {}
+api.base = 'http://api.are.na/v2'
+api.channels = `${api.base}/channels`
+api.user = id => `${api.base}/users/${id}`
+api.user.channels = id => `${api.base}/users/${id}/channels`
+api.channel = id => `${api.base}/channels/${id}`
+api.block = id => `${api.base}/blocks/${id}`
