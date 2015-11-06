a59a653290e4ccbe975fa5f8e8e41b71b302d34c
Nick <you@example.com>
2015-11-06 13:07:40 -0800

    view styles

--- Users/nickc/temp/empty/main.js
+++
@@ -1,8 +0,0 @@
-view Main {
-  <h1>Hello world!</h1>
-
-  $ = {
-    color: 'darkturquoise',
-    padding: 20
-  }
-}
--- viewStyles/main.js
+++ viewStyles/main.js
@@ -1,4 +1,9 @@
 view Main {
+  // take a look in kit.js where these views are defined
+  // its easy to build a "vocabulary" of views that you can
+  // easily use in your project that contain style and logic specific
+  // to what you're building
+  
   <H1>Hello world!</H1>
   <BigButton onClick={() => alert("welcome!")}>hello</BigButton>
   <br />
